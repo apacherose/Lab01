797932d2e2d4d5d1ff2f8b49aff6933d392d20c4
Velin <velin.achev@gmail.com>
2015-08-26 21:07:17 +0300

    bubble sort;

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -10,12 +10,43 @@ namespace Lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test Git");
+            
+            TestSorting.Bubble();
+
+            Console.Read();
         }
     }
 
-    class Test
+    class TestSorting 
     {
+        public static void Bubble()
+        {
+            int[] number = { 89, 76, 45, 92, 67, 12, 99 };
+            bool flag = true;
+            int temp;
+            int numLength = number.Length;
+            //sorting an array
+            for (int i = 1; (i <= (numLength - 1)) && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < (numLength - 1); j++)
+                {
+                    if (number[j + 1] > number[j])
+                    {
+                        temp = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+            //Sorted array
+            foreach (int num in number)
+            {
+                Console.Write("\t {0}", num);
+            }
 
+
+        }
     }
 }
